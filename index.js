820209262f5121a8ed9381bf9a2010dd6a21d7b1
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-03-14 13:25:17 -0400

    commit tree 701331dfe0880f2f0f14fbfce5993e02ebfdd6d8

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+
+
+function moveDodgerRight() {
+const leftNumbers = dodger.style.left.replace('px', '');
+const left = parseInt(leftNumbers, 10);
+
+if (left >= 360) {
+dodger.style.left = `360px`}
+else if (left >= 180) {dodger.style.left = `${left + 1}px`}
+}
+
+document.addEventListener('keydown', e => {if (e.key === "ArrowRight") {moveDodgerRight()}})
+
